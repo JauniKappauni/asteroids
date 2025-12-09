1344bf4159f73b4c5944af89a61c7c8441c85b06
JauniKappauni <social.jauni@gmail.com>
2025-12-09 13:49:33 +0100

    feat: implemented movement

--- main.cpp
+++ main.cpp
@@ -6,10 +6,30 @@ int main()
     int screenHeight = 400;
     InitWindow(screenWidth, screenHeight, "Asteroids");
     SetTargetFPS(60);
+    float posX = 20, posY = 20;
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
+        float dt = GetFrameTime();
+        float speed = 400.0f;
+        if (IsKeyDown(KEY_A))
+        {
+            posX -= speed * dt;
+        }
+        if (IsKeyDown(KEY_D))
+        {
+            posX += speed * dt;
+        }
+        if (IsKeyDown(KEY_W))
+        {
+            posY -= speed * dt;
+        }
+        if (IsKeyDown(KEY_S))
+        {
+            posY += speed * dt;
+        }
+        DrawRectangle(posX, posY, 20, 20, WHITE);
         EndDrawing();
     }
     CloseWindow();
